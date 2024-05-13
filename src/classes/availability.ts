5b0fa3875735d086ddaee2b906f6a115cc3a431d
aircongo <natelufuluabo@yahoo.ca>
2024-05-13 09:03:35 -0400

    added the id attribut

--- src/classes/availability.ts
+++ src/classes/availability.ts
@@ -1,10 +1,12 @@
 export class Availability {
     private date: string;
     private time: string;
+    private id: string;
 
     constructor(date: string, time: string) {
         this.date = date;
         this.time = time;
+        this.id = '';
     }
 
     public getDate(): string {
@@ -21,5 +23,13 @@ export class Availability {
 
     public setTime(newTime: string): void {
         this.time = newTime;
+    }
+
+    public getId(): string {
+        return this.id;
+    }
+
+    public setId(newId: string): void {
+        this.id  = newId;
     }
 }
