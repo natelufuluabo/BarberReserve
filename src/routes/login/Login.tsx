9a1f740a9289d595c740d8aefb32cc27a3324940
aircongo <natelufuluabo@yahoo.ca>
2024-09-27 19:46:51 -0400

    implemented the ability to capture form data

--- src/routes/login/Login.tsx
+++ src/routes/login/Login.tsx
@@ -2,17 +2,17 @@ import './Login.css'
 import TopBar from '../../topbar/TopBar'
 
 function Login() {
-
+  function handleSubmit() {}
   return (
     <>
       <TopBar />
       <div className="login-page">
         <div className="form">
           <h1>Connexion</h1>
-          <form className="login-form">
+          <form className="login-form" onSubmit={handleSubmit}>
             <input type="text" placeholder="courriel"/>
             <input type="password" placeholder="mot de passe"/>
-            <button>Se connecter</button>
+            <button type='submit'>Se connecter</button>
             <p className="message">Pas de compte? <a href="/signup">Créer un compte</a></p>
           </form>
         </div>
--- src/routes/signup/Signup.tsx
+++ src/routes/signup/Signup.tsx
@@ -1,7 +1,29 @@
 import './Signup.css'
+import { useState } from 'react'
 import TopBar from '../../topbar/TopBar'
 
 function Signup() {
+  type Barber = {
+    nomComplet: string;
+    courriel: string;
+    telephone: number;
+    motDePasse: string;
+  }
+  const [formData, setFormData] = useState<Barber>({
+    nomComplet: '', courriel: '', telephone: 5555555555, motDePasse: ''
+  });
+
+  function handleFormChange(event:any) {
+    setFormData({ ...formData, [event.target.name] : event.target.value })
+  }
+
+  function handleSubmit(event:any) {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({
+      nomComplet: '', courriel: '', telephone: 5555555555, motDePasse: ''
+    })
+  }
 
   return (
     <>
@@ -9,12 +31,12 @@ function Signup() {
       <div className="signup-page">
         <div className="form">
           <h1>Créer un compte</h1>
-          <form className="signup-form">
-            <input type="text" placeholder="nom complet"/>
-            <input type="email" placeholder="courriel"/>
-            <input type="number" placeholder="téléphone"/>
-            <input type="password" placeholder="mot de passe"/>
-            <button>Créer mon compte</button>
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <input name='nomComplet' type="text" placeholder="nom complet" onChange={event => handleFormChange(event)}/>
+            <input name='courriel' type="email" placeholder="courriel" onChange={event => handleFormChange(event)}/>
+            <input name='telephone' type="number" placeholder="téléphone" onChange={event => handleFormChange(event)}/>
+            <input name='motDePasse' type="password" placeholder="mot de passe" onChange={event => handleFormChange(event)}/>
+            <button type='submit'>Créer mon compte</button>
             <p className="message">Vous avez déjà un compte? <a href="/login">Connectez-vous</a></p>
           </form>
         </div>
